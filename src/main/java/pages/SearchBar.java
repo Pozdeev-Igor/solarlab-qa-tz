6b86c472c76a8f1c67fd07cff13fe64843db8120
Igor <Sketch_90@mail.ru>
2022-07-26 12:36:16 +0300

    change xPath to CSS selectors 1

--- src/main/java/pages/SearchBar.java
+++ src/main/java/pages/SearchBar.java
@@ -2,10 +2,11 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class SearchBar {
-    private final SelenideElement  searchButton = $x("//*[@id=\"dnn_ctr1253_HtmlModule_lblContent\"]/div/div/div[1]/a");
+    private final SelenideElement  searchButton = $("a[href*=\"poisk-223-fz\"]");
 
     public void clcButton(String link) {
         searchButton.click();
